47a41211994a2ad8be4a46755f7575995c9de059
GuillaumePitonInrae <74602215+GuillaumePitonInrae@users.noreply.github.com>
2022-05-05 22:59:22 +0200

    01_MainCode.R is the main code to be used.
    It help the user to load the source codes, to define where are the input data, to define the type of simulation to be run and where to store the results

--- 0SourceCodes/01_MainCode.R
+++ 0SourceCodes/01_MainCode.R
@@ -193,7 +193,7 @@ while(Perform.Another.Simulation=="yes")
                                               ,Boulders$Boulder_size_category_.m.
                                               ," m in a volume of "
                                               ,Boulders$Reference_Volume," m3 of deposit"))
-    #Pop up windows to define the vales
+    #Pop up windows to define the values
     if(Adjust.event.manually){
       for(j in c(1:length(input)))
       {
