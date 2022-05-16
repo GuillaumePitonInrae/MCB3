304527547ea29ed889cd85af41410d63feb00b37
GuillaumePitonInrae <74602215+GuillaumePitonInrae@users.noreply.github.com>
2022-05-16 17:58:07 +0200

    Update to install codes to account for the HYRISK package being removed from CRAN
    The new version directly download the source codes and ask the user to locate the downloaded archive.

--- 0SourceCodes/00_InstallPackages.R
+++ 0SourceCodes/00_InstallPackages.R
@@ -4,9 +4,17 @@
 # install packages
 install.packages("lubridate") #To add date on plot
 install.packages("stringr") #To replace string
-install.packages("HYRISK")#Package uncertainty propagation
 install.packages("svDialogs") #Package for popup dialog windows
 install.packages("ggplot2") #Plots
-install.packages("gridExtra") #For multi ggplots
-install.packages("grid") #For multi ggplots
-install.packages("dplyr") #For data manipulation
+
+# Then download the HYRISK package here: 
+browseURL("https://cran.r-project.org/src/contrib/Archive/HYRISK/HYRISK_1.2.tar.gz")
+
+library(svDialogs) #Package for popup dialog windows
+#Selecting the repository where the HYRISK package HYRISK_1.2.tar.gz is stored
+dlg_message(message="Show me where was downloaded the Hyrisk package (file HYRISK_1.2.tar.gz)"
+            , type = c("ok"));DownloadedPackage<-dlg_open(default = "HYRISK_1.2.tar.gz",
+                                                          title = "Show me where was downloaded the Hyrisk package (file HYRISK_1.2.tar.gz)")$res
+
+install.packages(DownloadedPackage, repos = NULL, type = "source")#Package uncertainty propagation
+
